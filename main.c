7f7699c88fe60fc934d3b8c8a0e1acd67cb76dc5
Martinjfs <146488448+Martinjfs@users.noreply.github.com>
2024-01-19 16:06:19 -0500

    Update main.c

--- main.c
+++ main.c
@@ -4,29 +4,52 @@
 
 int main() {
     const char *nombreArchivo = "archivo1";
+    int opcion;
 
     cargaInicial(nombreArchivo);
 
-    char productoBuscar[50];
-    printf("Ingrese el nombre del producto a buscar: ");
-    scanf("%s", productoBuscar);
+    do {
+        printf("\n=== Menú ===\n");
+        printf("1. Buscar producto\n");
+        printf("2. Agregar producto\n");
+        printf("3. Borrar producto\n");
+        printf("4. Salir\n");
+        printf("Ingrese la opción: ");
+        scanf("%d", &opcion);
 
-    buscarProducto(nombreArchivo, productoBuscar);
+        switch (opcion) {
+            case 1: {
+                char productoBuscar[50];
+                printf("Ingrese el nombre del producto a buscar: ");
+                scanf("%s", productoBuscar);
+                buscarProducto(nombreArchivo, productoBuscar);
+                break;
+            }
+            case 2: {
+                char nombreProducto[100];
+                int cantidadProducto;
+                printf("Ingrese el nombre del producto a agregar: ");
+                scanf(" %[^\n]s", nombreProducto);
+                printf("Ingrese la cantidad del producto a agregar: ");
+                scanf("%d", &cantidadProducto);
+                agregarDatos(nombreArchivo, nombreProducto, cantidadProducto);
+                break;
+            }
+            case 3: {
+                char productoBorrar[50];
+                printf("Ingrese el nombre del producto a borrar: ");
+                scanf("%s", productoBorrar);
+                borrarRegistro(nombreArchivo, productoBorrar);
+                break;
+            }
+            case 4:
+                printf("Saliendo del programa.\n");
+                break;
+            default:
+                printf("Opción no válida. Intente de nuevo.\n");
+        }
 
-    char nombreProducto[100]; //Permite colocar mas palabras
-    int cantidadProducto;
-    printf("Ingrese el nombre del producto a agregar: ");
-    scanf(" %[^\n]s", nombreProducto); // lee las lineas con espacios 
-    printf("Ingrese la cantidad del producto a agregar: ");
-    scanf("%d", &cantidadProducto);
-
-    agregarDatos(nombreArchivo, nombreProducto, cantidadProducto);
-
-    char productoBorrar[50];
-    printf("Ingrese el nombre del producto a borrar: ");
-    scanf("%s", productoBorrar);
-
-    borrarRegistro(nombreArchivo, productoBorrar);
+    } while (opcion != 4);
 
     return 0;
 }
